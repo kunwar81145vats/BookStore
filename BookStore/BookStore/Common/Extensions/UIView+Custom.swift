cd5b8db48a0de951b376f1b28ae43bb1f8fe506f
Iricher Supera <irsupera@yahoo.com>
2022-07-18 09:56:36 -0400

    Commit: Padding for textfields Details: Custom class of UITextField created to created padded textfields.

--- BookStore/BookStore/Common/Extensions/UIView+Custom.swift
+++ BookStore/BookStore/Common/Extensions/UIView+Custom.swift
@@ -33,3 +33,23 @@ override public func layoutSubviews() {
    clipsToBounds = true
   }
 }
+
+class PaddedTextField: UITextField
+{
+    var textPadding = UIEdgeInsets(
+          top: 0,
+          left: 20,
+          bottom: 0,
+          right: 0
+      )
+
+      override func textRect(forBounds bounds: CGRect) -> CGRect {
+          let rect = super.textRect(forBounds: bounds)
+          return rect.inset(by: textPadding)
+      }
+
+      override func editingRect(forBounds bounds: CGRect) -> CGRect {
+          let rect = super.editingRect(forBounds: bounds)
+          return rect.inset(by: textPadding)
+      }
+}
